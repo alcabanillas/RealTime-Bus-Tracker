5def8c125090dc108dd57e746c1d75015bb2e192
alcabanillas <acabanillas@gmail.com>
2022-08-09 20:48:07 +0200

    add h8

--- mapanimation.js
+++ mapanimation.js
@@ -21,9 +21,48 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYWxjYWJhbmlsbGFzIiwiYSI6ImNsNmpuNmV4dzA5Y3YzY
 let map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [-71.104081, 42.365554],
+  center: [2.1906674667514117,41.4434932054511],
   zoom: 14,
 });
+
+
+async function run(){
+  // get bus data    
+  const locations = await getBusLocations();
+  console.log(new Date());
+  console.log(locations);
+
+  const markers = [];
+
+
+
+  locations.forEach(element => {
+    const title = element.properties.NOM_PARADA;
+
+    var marker = new mapboxgl.Marker(
+      {color: "#00FF00"}
+    )
+    .setLngLat([element.geometry.coordinates[0], element.geometry.coordinates[1]])
+    .addTo(map);
+  });
+
+  map.addLayer(markers);
+
+  // timer
+  //setTimeout(run, 15000);
+}
+
+const URL_H8 = 'https://api.tmb.cat/v1/transit/linies/bus/208/parades?app_id=de5dd563&app_key=008dfbd63205bd063ab1dcc8493fbf2f';
+const URL_H6 = 'https://api.tmb.cat/v1/transit/linies/bus/206/parades?app_id=de5dd563&app_key=008dfbd63205bd063ab1dcc8493fbf2f';
+
+
+// Request bus data from TMB
+async function getBusLocations(){
+  const url = URL_H8;
+  const response = await fetch(url);
+  const json     = await response.json();
+  return json.features;
+}
 
 // TODO: add a marker to the map at the first coordinates in the array busStops. The marker variable should be named "marker"
 
@@ -47,7 +86,4 @@ function move() {
 
 }
 
-// Do not edit code past this point
-if (typeof module !== 'undefined') {
-  module.exports = { move };
-}
+run();
