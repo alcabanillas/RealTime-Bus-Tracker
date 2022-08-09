592b0b1a4213c2d2fc5232e00ffba1f9c5f42bb5
alcabanillas <acabanillas@gmail.com>
2022-08-10 01:20:04 +0200

    boston ok

--- mapanimation.js
+++ mapanimation.js
@@ -6,21 +6,28 @@ const map = L.mapbox.map('map')
 
 const markers = [];
 
-async function run(){
+function generateRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+async function move(){
   // get bus data    
   const locations = await getBusLocations();
 
   console.log(new Date());
-  console.log(locations);
 
   locations.forEach( vehicle => {
     let currvehicle = markers.filter( (element) => element.id == vehicle.id);
-    
     let marker = null;
 
     if (currvehicle.length == 0) {
       marker = L.marker(new L.LatLng(vehicle.attributes.latitude, vehicle.attributes.longitude), {
-        icon: L.mapbox.marker.icon({'marker-symbol': 'post', 'marker-color': '0044FF'}),
+        icon: L.mapbox.marker.icon({'marker-symbol': 'bus', 'marker-color': generateRandomColor()}),
         title: vehicle.id
       });
 
@@ -33,20 +40,19 @@ async function run(){
       marker.setLatLng([vehicle.attributes.latitude, vehicle.attributes.longitude]);
     }
     marker.addTo(map);
-    marker.bindPopup(vehicle.id);
+    marker.bindPopup(vehicle.attributes.label);
   })
 
   // timer
-  setTimeout(run, 15000);
+  setTimeout(move, 15000);
 }
 
 
 // Request bus data from MBTA
 async function getBusLocations(){
-	var url = 'https://api-v3.mbta.com/vehicles?api_key=ca34f7b7ac8a445287cab52fb451030a&filter[route]=1&include=trip';	
+  var url = 'https://api-v3.mbta.com/vehicles?api_key=ca34f7b7ac8a445287cab52fb451030a&filter[route]=1&include=trip';	
 	var response = await fetch(url);
 	var json     = await response.json();
 	return json.data;
 }
 
-run();
