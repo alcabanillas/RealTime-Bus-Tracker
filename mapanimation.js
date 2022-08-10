539e8bacd20abd0314c8db0335501ae916c25888
alcabanillas <acabanillas@gmail.com>
2022-08-10 09:48:38 +0200

    usar promise + capa de pintado de refresco de datos

--- mapanimation.js
+++ mapanimation.js
@@ -4,6 +4,13 @@ const map = L.mapbox.map('map')
   .setView([42.365554, -71.104081], 14)
   .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
 
+
+map.on('style.load', () => {
+  map.setFog({}); // Set the default atmosphere style
+});
+
+L.control.scale().addTo(map);
+
 const markers = [];
 
 function generateRandomColor() {
@@ -15,13 +22,8 @@ function generateRandomColor() {
   return color;
 }
 
-async function move(){
-  // get bus data    
-  const locations = await getBusLocations();
-
-  console.log(new Date());
-
-  locations.forEach( vehicle => {
+function paintBuses(data) {
+  data.forEach( vehicle => {
     let currvehicle = markers.filter( (element) => element.id == vehicle.id);
     let marker = null;
 
@@ -41,8 +43,18 @@ async function move(){
     }
     marker.addTo(map);
     marker.bindPopup(vehicle.attributes.label);
-  })
+  });
+  var t = new Date();
+  console.log(t);
+  document.getElementById('datainfo').innerHTML = 'Data info: ' +  t.getHours() + ':' + t.getMinutes() + ':' + t.getSeconds();
+}
 
+function move(){
+  // get bus data    
+  const locations = getBusLocations;
+  locations()
+    .then( (data) => { paintBuses(data); } )
+    .catch( (err) => { console.error('data error', err); })
   // timer
   setTimeout(move, 15000);
 }
@@ -50,7 +62,8 @@ async function move(){
 
 // Request bus data from MBTA
 async function getBusLocations(){
-  var url = 'https://api-v3.mbta.com/vehicles?api_key=ca34f7b7ac8a445287cab52fb451030a&filter[route]=1&include=trip';	
+  //var url = 'https://api-v3.mbta.com/vehicles?api_key=ca34f7b7ac8a445287cab52fb451030a&filter[route]=1&include=trip';	
+  var url = 'https://api-v3.mbta.com/vehicles?api_key=ca34f7b7ac8a445287cab52fb451030a';	
 	var response = await fetch(url);
 	var json     = await response.json();
 	return json.data;
